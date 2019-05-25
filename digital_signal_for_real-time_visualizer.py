0e3df552c2198702d5cad3bf3c0901d86f9e6cc7
Chia-Jung Yeh <49973801+yeh8211TK@users.noreply.github.com>
2019-05-25 23:47:23 +0800

    Add files via upload

---
+++ digital_signal_for_real-time_visualizer.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+import numpy as np
+import pyaudio 
+import matplotlib.pyplot as plt
+
+fs = 11000
+CHUNK = 1024
+pa = pyaudio.PyAudio()
+stream = pa.open(format = pyaudio.paInt16, channels = 1, rate = fs, input = True, 
+                output = False, frames_per_buffer = CHUNK)
+
+try:
+    while True:
+        data = stream.read(CHUNK)
+        x = np.fromstring(data, np.int16)
+        
+        plt.clf()
+        plt.plot(x)
+        plt.axis([0, CHUNK, -3000, 3000])
+        
+        plt.pause(0.1)
+        
+except KeyboardInterrupt:
+    print("Quit")
+    pa.close(stream)
+    quit()
+
+
+# In[ ]:
+
+
+
